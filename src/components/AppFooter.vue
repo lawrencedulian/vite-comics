3ba11f775f77f269667fb9083064d190770cd77e
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-08 17:38:31 +0100

    AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "AppFooter",
+    data() {
+        return {
+
+        }
+    }
+}    
+</script>
+
+<template>
+    <footer>
+        <div class="footer-top">
+
+        </div>
+        <div class="footer-middle">
+
+        </div>
+        <div class="footer-bottom">
+
+        </div>
+    </footer>
+</template>
+
+<style>
+
+</style>
