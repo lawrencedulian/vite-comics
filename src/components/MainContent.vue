cce256a57444f493a34d267e46482f331a9a7e10
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-08 17:38:02 +0100

    MainContent

---
+++ src/components/MainContent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "MainContent",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <h1>Content Goes Here</h1>
+        </div>
+    </main>
+
+</template>
+
+<style>
+h1 {
+    color: white;
+}
+</style>
